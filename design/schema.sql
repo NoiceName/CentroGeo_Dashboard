e7b3d875b67edb5f7aad4697434433629154b92d
Maksym Koval <m.koval@student.utwente.nl>
2020-05-22 15:01:18 +0200

    Created basic simulation and snapshot schema

---
+++ design/schema.sql
@@ -0,0 +1,20 @@
+DROP SCHEMA if exists "projectSchema" cascade;
+
+CREATE SCHEMA "projectSchema"; 
+
+create table "projectSchema".Simulation(
+	simulation_id serial constraint simulation_id_pk primary key,
+	name text,
+	date date,
+	tags text,
+	description text
+);
+
+create table "projectSchema".Snapshot(
+	snapshot_id serial constraint snapshot_id_pk primary key,
+	simulation_id serial constraint simulation_id_fk not null 
+		references "projectSchema".Simulation (simulation_id) on delete cascade,
+	time real
+);
+
+
