eb6730921165981701cdca3237c3e76587c0a7c2
Maksym Koval <m.koval@student.utwente.nl>
2020-05-22 17:07:49 +0200

    Added tables SnapshotVehicle, Vehicle, SnapShotLane

--- design/schema.sql
+++ design/schema.sql
@@ -24,8 +24,37 @@ create table "projectSchema".Lane(
 	shape text
 );
 
+create table "projectSchema".SnapshotLane(
+	snapshot_lane_id serial constraint snapshot_lane_id_pk primary key,
+	lane text constraint lane_fk not null
+		references "projectSchema".Lane (lane_id) on delete cascade,
+	snapshot serial constraint snapshot_fk not null
+		references "projectSchema".Snapshot (snapshot_id) on delete cascade
+);
+
 create table "projectSchema".User(
 	user_id serial constraint user_id_pk primary key,
 	password_hash text
 );
 
+
+create table "projectSchema".Vehicle(
+	vehicle_id text constraint vehicle_id_pk primary key,
+	depart real
+);
+
+create table "projectSchema".SnapshotVehicle(
+	snapshot_vehicle_id serial constraint snapshot_vehicle_id_pk primary key,
+	vehicle text constraint vehicle_fk not null
+		references "projectSchema".Vehicle (vehicle_id) on delete cascade,
+	snapshot serial constraint snapshot_fk not null
+		references "projectSchema".Snapshot (snapshot_id) on delete cascade,
+	snapshot_lane serial constraint snapshot_lane_fk not null
+		references "projectSchema".SnapshotLane (snapshot_lane_id) on delete cascade,
+	type text,
+	speedFactor real, 
+	state text,
+	pos float, 
+	speed float,
+	posLat float
+);
