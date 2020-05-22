04b07bdbe7e89cfa7921535037c054e183886a84
Maksym Koval <m.koval@student.utwente.nl>
2020-05-22 18:36:33 +0200

    Uploaded class diagram + schema can now be minimally used

--- design/schema.sql
+++ design/schema.sql
@@ -1,60 +1,60 @@
-DROP SCHEMA if exists "projectSchema" cascade;
+drop schema if exists "projectschema" cascade;
 
-CREATE SCHEMA "projectSchema"; 
+create schema "projectschema"; 
 
-create table "projectSchema".Simulation(
-	simulation_id serial constraint simulation_id_pk primary key,
+create table "projectschema".simulation(
+	simulation_id serial primary key,
 	name text,
 	date date,
 	tags text,
 	description text
 );
 
-create table "projectSchema".Snapshot(
-	snapshot_id serial constraint snapshot_id_pk primary key,
-	simulation_id serial constraint simulation_id_fk not null 
-		references "projectSchema".Simulation (simulation_id) on delete cascade,
+create table "projectschema".snapshot(
+	snapshot_id serial primary key,
+	simulation integer not null 
+		references "projectschema".simulation (simulation_id) on delete cascade,
 	time real
 );
 
-create table "projectSchema".Lane(
-	lane_id text constraint lane_id_pk primary key,
+create table "projectschema".lane(
+	lane_id text primary key,
 	index integer,
 	length real,
 	shape text
 );
 
-create table "projectSchema".SnapshotLane(
-	snapshot_lane_id serial constraint snapshot_lane_id_pk primary key,
-	lane text constraint lane_fk not null
-		references "projectSchema".Lane (lane_id) on delete cascade,
-	snapshot serial constraint snapshot_fk not null
-		references "projectSchema".Snapshot (snapshot_id) on delete cascade
+create table "projectschema".snapshotlane(
+	snapshot_lane_id serial primary key,
+	lane text not null
+		references "projectschema".lane (lane_id) on delete cascade,
+	snapshot integer not null
+		references "projectschema".snapshot (snapshot_id) on delete cascade
 );
 
-create table "projectSchema".User(
-	user_id serial constraint user_id_pk primary key,
+create table "projectschema".user(
+	user_id serial primary key,
 	password_hash text
 );
 
 
-create table "projectSchema".Vehicle(
-	vehicle_id text constraint vehicle_id_pk primary key,
+create table "projectschema".vehicle(
+	vehicle_id text primary key,
 	depart real
 );
 
-create table "projectSchema".SnapshotVehicle(
-	snapshot_vehicle_id serial constraint snapshot_vehicle_id_pk primary key,
+create table "projectschema".snapshotvehicle(
+	snapshot_vehicle_id serial primary key,
 	vehicle text constraint vehicle_fk not null
-		references "projectSchema".Vehicle (vehicle_id) on delete cascade,
-	snapshot serial constraint snapshot_fk not null
-		references "projectSchema".Snapshot (snapshot_id) on delete cascade,
-	snapshot_lane serial constraint snapshot_lane_fk not null
-		references "projectSchema".SnapshotLane (snapshot_lane_id) on delete cascade,
+		references "projectschema".vehicle (vehicle_id) on delete cascade,
+	snapshot integer constraint snapshot_fk not null
+		references "projectschema".snapshot (snapshot_id) on delete cascade,
+	snapshot_lane integer not null
+		references "projectschema".snapshotlane (snapshot_lane_id) on delete cascade,
 	type text,
-	speedFactor real, 
+	speedfactor real, 
 	state text,
 	pos float, 
 	speed float,
-	posLat float
+	poslat float
 );
