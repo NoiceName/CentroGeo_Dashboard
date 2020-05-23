4d19a3d4f04fbb88dfad0624a5e897164b01c5f4
Maksym Koval <m.koval@student.utwente.nl>
2020-05-23 21:45:06 +0200

    Wrote up Metadata and added a get for simulation resource + basic html

--- src/main/java/resources/simulations/SimulationsResource.java
+++ src/main/java/resources/simulations/SimulationsResource.java
@@ -1,5 +1,29 @@
 package resources.simulations;
 
-public class SimulationsResource {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+
+@Path("/simulation")
+public class SimulationsResource {
+	
+	@GET
+	@Produces("text/html")
+	public void getSimulationsPage(@Context HttpServletResponse response) {
+		forwardUser("../viewing-simulations/display.html", response);
+	}
+	
+	public void forwardUser(String path, HttpServletResponse response) {
+		try {
+			response.sendRedirect(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
