9cf90517d9d749e779859b18f05f76b4dc2b1f57
Maksym Koval <m.koval@student.utwente.nl>
2020-05-26 22:31:14 +0200

    Added a test for checkCookie()

--- src/test/java/cookie_manager/CookieManagerTest.java
+++ src/test/java/cookie_manager/CookieManagerTest.java
@@ -2,10 +2,12 @@ package cookie_manager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 
-import cookie_manager.exceptions.CannotCreateCookieException;
 import dao.CookieDAO;
+import dao.UserDAO;
 import model.User;
 
 class CookieManagerTest {
@@ -14,16 +16,33 @@ class CookieManagerTest {
 	@Test
 	void assignCookieTest() {
 		User user = new User();
-		CookieManager manager = new CookieManager();
-		try {
-			manager.assignCookie(user);
-		} catch (CannotCreateCookieException e) {
-			e.printStackTrace();
-		}
+		CookieManager.assignCookie(user);
 		String result = CookieDAO.instance.getModel().get(user);
 		assertTrue(result instanceof String);
 	}
-
 	
+	@Test
+	void testIfCookiesExists() {
+		User user1 = new User("max", "amazingPassword");
+		User user2 = new User("sam", "123456");
+		
+		//Create new users 
+		HashMap<String, User> userMap = (HashMap<String, User>) UserDAO.instance.getModel();
+		userMap.put(user1.getUserName(), user1);
+		userMap.put(user2.getUserName(), user2);
+
+		User max = userMap.get("max");
+		User sam = userMap.get("sam");
+		
+		
+		//Assing cookie tokens to users
+		CookieManager.assignCookie(max);
+		CookieManager.assignCookie(sam);
+		
+		//Test whether the given tokens match
+		assertTrue(CookieManager.checkCookie(max, CookieDAO.instance.getModel().get(max)));
+		assertFalse(CookieManager.checkCookie(sam, CookieDAO.instance.getModel().get(max)));
+	
+	}	
 	
 }
