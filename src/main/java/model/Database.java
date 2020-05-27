b4afad7c01ef17521e16c35c9628112691c3289f
Maksym Koval <m.koval@student.utwente.nl>
2020-05-27 13:18:51 +0200

    Now able to connect to the database and through the database class

---
+++ src/main/java/model/Database.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Database {
+	//Specify the parameters for the database here!
+	//The URL should be of the following format jdbc:postgresql://localhost/test
+	String url = "";
+	String username = ""; 
+	String password = "";
+
+	/**
+	 * Saves a database with given parameters
+	 * @param url
+	 * @param username
+	 * @param password
+	 */
+	public Database(String url, String username, String password) {
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+	
+	/**
+	 * Instantiates a database with default parameters
+	 */
+	public Database() {
+	}
+	
+	/**
+	 * Loads the driver for a pgsql database.
+	 */
+	public static void loadPGSQL(){
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Connects to the specified database
+	 * @return
+	 */
+	public Connection connect() {
+		Properties props = new Properties();
+		props.setProperty("user", this.username);
+		props.setProperty("password", this.password);
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(this.url, props);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+
+
+}
