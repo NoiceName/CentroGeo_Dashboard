57c0f39d29e7089bb353a71a387cebd89b40de8d
Liping_Yin <l.yin@student.utwente.nl>
2020-05-27 20:19:09 +0200

    connect user data into database

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,47 +1,93 @@
 package dao;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import java.sql.*;
 
 import model.User;
 
 public enum UserDAO {
 	instance;
-	private Map<String, User> users = new HashMap<String, User>();
+//	private Map<String, User> users = new HashMap<String, User>();
 	
 	private UserDAO() {
-		User user1 = new User("user1","123456");
-		users.put("user1", user1);
+//		User user1 = new User("user1","123456");
+//		users.put("user1", user1);
 	}
 	
-	public Map<String, User> getModel() {
-		return users;
+//	public Map<String, User> getModel() {
+//		return users;
+//	}
+	
+	public String getUserPassword(String userName){
+		PreparedStatement query = null;
+		String returnPassword = null;
+		Connection conn = null;
+		
+		try {
+			conn = databaseConnection.getConnection();
+			query = conn.prepareStatement("select Password from projectschema.user "+
+			"where UserName = ?; ");
+			query.setString(1, userName);
+			ResultSet rs = query.executeQuery();
+			// check if the user in the table and is with a correct password
+			while(rs.next()) {
+				returnPassword = rs.getString("Password");
+			}
+			query.close();		
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			
+			if(conn!=null){
+			   try {
+				 
+				   conn.close();
+				
+			   } catch (SQLException e) {
+				e.printStackTrace();
+			   }
+			}
+		}	    
+		return returnPassword;
 	}
 	
-//	public User getUser(String name){
-//		   
-//		      for(Entry<String, User> user: users.entrySet()){
-//		         if(user.getKey() == name){
-//		            return user.getValue();
-//		         }
-//		      }
-//		      return null;
-//		   }
-//
-//	 public int addUser(User pUser){
-//		      
-//		      boolean userExists = false;
-//		      for(Entry<String, User> user: users.entrySet()){
-//		         if(user.getKey() == pUser.getName()){
-//		            userExists = true;
-//		            break;
-//		         }
-//		      }		
-//		      if(!userExists){
-//		         users.put(pUser.getName(),pUser);
-//		         return 1; // add successfully
-//		      }
-//		      return 0;
-//		   }
+	public int insertUser(User user)throws Exception {
+		/**
+		 * return number
+		 * 0:User sql exception, data not entered
+		 * 1:user data entered successfully!
+		 * 2:user already exists, change a user name
+		 */
+		PreparedStatement query = null;
+		int returnString; // user data entered successfully!;
+		Connection conn = null;
+		
+		try {	
+			conn = databaseConnection.getConnection();
+			
+			if(getUserPassword(user.getUserName())!=null) {
+				
+				return 2; // user already exists
+			}
+			
+			String sql = "insert into projectschema.user "+
+					"values(?,?); ";
+			query = conn.prepareStatement(sql);
+			query.setString(1, user.getUserName());
+			query.setString(2, user.getPassword());
+			returnString = query.executeUpdate();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			returnString = 0; // User data not entered;
+		}
+		finally {
+			query.close();
+			conn.close();
+		}
+		return returnString;		
+	}	
 }
+
+
