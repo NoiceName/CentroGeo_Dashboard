d31cd16135150f8d0d09ee96e037a5cf3a654405
Samuel <s.f.coste@student.utwente.nl>
2020-05-28 06:28:53 -0300

    Unbroken the function

--- src/main/webapp/Graph visualization/importXML.js
+++ src/main/webapp/Graph visualization/importXML.js
@@ -8,37 +8,40 @@
 function getXML() {
         console.log("retrieving xml");
 
-			//Statically set simulation id !!! that is sent to the server
-			var jsonObject = {'simulation':1};
-			var json = JSON.stringify(jsonObject);
+ //    $(function () {     $('#chartGen').click(function(event) { 
+	// 		//Statically set simulation id !!! that is sent to the server
+	// 		var jsonObject = {'simulation':1};
+	// 		var json = JSON.stringify(jsonObject);
 
-		$.ajax({
-			url: '/CentroGeo/Resources/metadata/states',
-			data: json,
-			//try with application/json later
-			type: 'POST',
-			dataType: 'json',
-			contentType : 'application/json',
-			success: function (resp) {success(resp)},
-			error: function(jqXHR, textStatus, errorThrown) {
-				alert('Cannot contact the server!');
-			}
-		});
+	// 	$.ajax({
+	// 		url: '/CentroGeo/Resources/metadata/states',
+	// 		data: json,
+	// 		//try with application/json later
+	// 		type: 'POST',
+	// 		dataType: 'json',
+	// 		contentType : 'application/json',
+	// 		success: function (resp) {success(resp)},
+	// 		error: function(jqXHR, textStatus, errorThrown) {
+	// 			alert('Cannot contact the server!');
+	// 		}
+	// 	});
 		
 
-	function success(resp) {
-		//resp is the data array
-        console.log("yeey");
-		console.log(resp[0].data);
-        dataArray = [];
+	// function success(resp) {
+	// 	//resp is the data array
+ //        console.log("yeey");
+	// 	console.log(resp[0].data);
+ //        dataArray = [];
 
-        for (var i = 0; i < resp.length; i++) {
-            dataArray[i] = resp[i].data;
-        }
+ //        for (var i = 0; i < resp.length; i++) {
+ //            dataArray[i] = resp[i].data;
+ //        }
 
-		return dataArray;
-	}
-
+	// 	return dataArray;
+	// }
+    
+ //    }); 
+ //    });
 
     
 }
--- src/main/webapp/Graph visualization/script.js
+++ src/main/webapp/Graph visualization/script.js
@@ -179,10 +179,40 @@ function populateLaneSelect() {
 }
 
 
-$(function () {     $('#chartGen').click(function(event) {         
-// Statically set simulation id !!! that is sent to the server  
-console.log("get that data");       
-var jsonObject = {'simulation':1};         var json = JSON.stringify(jsonObject);      $.ajax({         url: '/CentroGeo/Resources/metadata/states',         data: json,         //try with application/json later         
-  type: 'POST',         dataType: 'json',         contentType : 'application/json',         success: function (resp) {success(resp)},         error: function(jqXHR, textStatus, errorThrown) {             alert('Cannot contact the server!');         }     });     }); });
 
+
+ $(function () {     $('#chartGen').click(function(event) { 
+      //Statically set simulation id !!! that is sent to the server
+      var jsonObject = {'simulation':1};
+      var json = JSON.stringify(jsonObject);
+
+    $.ajax({
+      url: '/CentroGeo/Resources/metadata/states',
+      data: json,
+      //try with application/json later
+      type: 'POST',
+      dataType: 'json',
+      contentType : 'application/json',
+      success: function (resp) {success(resp)},
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert('Cannot contact the server!');
+      }
+    });
+    
+
+  function success(resp) {
+    //resp is the data array
+        console.log("yeey");
+    console.log(resp[0].data);
+        dataArray = [];
+
+        for (var i = 0; i < resp.length; i++) {
+            dataArray[i] = resp[i].data;
+        }
+
+    return dataArray;
+  }
+    
+    }); 
+    });
 
