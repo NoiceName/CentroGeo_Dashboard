21d754268789043208577a9cd0b4876d496989c0
Maksym Koval <m.koval@student.utwente.nl>
2020-06-07 12:56:06 +0200

    Edited Reinouts code, now able to upload a zip file with new interface

--- src/main/webapp/uploadzip/zip.js
+++ src/main/webapp/uploadzip/zip.js
@@ -1,41 +1,46 @@
-const zipFile = document.getElementById("zip-file");
-const zipButton = document.getElementById("zip-button");
-const customText = document.getElementById("custom-text");
+$(function() {
+	console.log('updated21')
+	const zipFile = document.getElementById("zip-file");
+	const zipButton = document.getElementById("zip-button");
+	const customText = document.getElementById("custom-text");
 
-zipButton.addEventListener("click", function() {
-	zipFile.click();
-});
+	zipButton.addEventListener("click", function() {
+		zipFile.click();
+	});
 
-zipFile.addEventListener("change", function() {
-	if(zipFile.value) {
-		customText.innerHTML = zipFile.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-	} else {
-		customText.innerHTML = "no file chosen";
-	}
-});
+	zipFile.addEventListener("change", function() {
+		if(zipFile.value) {
+			customText.innerHTML = zipFile.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+		} else {
+			customText.innerHTML = "no file chosen";
+		}
+	});
 
-$("#zipform").submit(function (evt) {
-    var formData = new FormData();
-    console.log(zipFile.files[0]);
-    // formData.append('zip', zipFile.files[0]);
-    formData = zipFile.files[0];
+	$("#zipform").submit(function (evt) {
+		console.log("files submitted");
+		var formData = new FormData();
+		console.log(zipFile.files[0]);
+		// formData.append('zip', zipFile.files[0]);
+		formData = zipFile.files[0];
 
-    $.ajax({
-        url: '/CentroGeo/resources/simulations',
-        type: 'POST',
-        data: formData,
-        async: false,
-        cache: false,
-        contentType: 'application/zip',
-        dataType: 'application/zip',
-        title: 'file',
-        enctype: 'application/zip',
-        processData: false,
-        success: function (response) {
-            alert(response);
-        }
-    });
-    return false;
+		$.ajax({
+			url: '/CentroGeo/resources/simulations',
+			type: 'POST',
+			data: formData,
+			async: false,
+			cache: false,
+			contentType: 'application/zip',
+			dataType: 'application/zip',
+			title: 'file',
+			enctype: 'application/zip',
+			processData: false,
+			success: function (response) {
+				alert(response);
+			}
+		});
+		return false;
+	});
+
 });
 
 
