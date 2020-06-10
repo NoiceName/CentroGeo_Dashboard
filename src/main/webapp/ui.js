9bf015147ed68f552fbebe8c7169de43461cfcb9
Maksym Koval <m.koval@student.utwente.nl>
2020-06-10 17:30:21 +0200

    Added some comments

--- src/main/webapp/ui.js
+++ src/main/webapp/ui.js
@@ -25,13 +25,14 @@ function clearInputFields(){
 $(function() {
 	var laneIdInput = $("#laneIdInput");
 
-
 	laneIdInput.keyup(function() {
 		var ids = getLanesId();
 		var jsLaneIdInput = document.getElementById("laneIdInput");
 		let input = jsLaneIdInput.value;
 		let p = document.getElementById('foundIdsContainer');
+		//If the input is longer than 3 characters
 		if(input.length >= 3) {
+			//Filter through the lane IDS in the simulation by the given input and append them as children to the foundIdsContainer
 			let filtered = getFilteredIds(input, ids);
 			filtered.forEach(function(item) {
 				createBadgeAndAdd(item,p);
@@ -45,11 +46,13 @@ $(function() {
 });
  
 //Removes badges from display
+//Not used
 function removeBadges(container){
 	var c = conainter.childNodes;	
 }
 
 //Id is a string of a lane;
+//Creates an appropriate <a> element and adds to to the specified container - html element
 function createBadgeAndAdd(id, container) {
 	var newBadge = document.createElement("a");
 	newBadge.classList.add('badge', 'badge-primary', 'badge-pill', 'foundId');
@@ -59,6 +62,7 @@ function createBadgeAndAdd(id, container) {
 	container.appendChild(newBadge);
 }
 
+//Transfer the id from Found IDs to Selected Ids container
 function selectBadge(Badge, parentElem) {
 	var transferContainer = document.getElementById('selectedIdsContainer');
 	Badge.removeEventListener('mousedown', arguments.callee);
@@ -66,6 +70,7 @@ function selectBadge(Badge, parentElem) {
 }
 
 
+//Returns an array of string ids of lanes that have been selected by the user.
 function getSelectedIds(){
 	var transferContainer = document.getElementById('selectedIdsContainer');
 	let children = transferContainer.children;
@@ -81,6 +86,11 @@ function getSelectedIds(){
 	return result;
 }
 
+
+//inputString - String
+//ids - Array of strings
+//For each string in the ids check if inputString is is part of it in the case that it is 
+//add it to the result.
 function getFilteredIds(inputString, ids) {
 		let filtered = ids.filter(function(str) {return str.includes(inputString)});
 		return filtered;
