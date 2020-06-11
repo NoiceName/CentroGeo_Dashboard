7c012ceb8f196337a6e4948a56dc91d95651d99e
Samuel <s.f.coste@student.utwente.nl>
2020-06-11 07:21:20 -0300

    Solved repeting founds in results

--- src/main/webapp/ui.js
+++ src/main/webapp/ui.js
@@ -3,11 +3,19 @@ $(function () {
 	$("#lineChartSelect").click(function () {
 		clearInputFields();
 		$("#lineChartOptions").removeClass('d-none');
+		clearSelectedAndFound();
+	});
+
+	$("#vehicleChartSelect").click(function () {
+		clearInputFields();
+		$("#vehicleChartOptions").removeClass('d-none');
+		clearSelectedAndFound();
 	});
 
 	$("#uselessChartSelect").click(function () {
 		clearInputFields();
 		$("#pieChartOptions").removeClass('d-none');
+		clearSelectedAndFound();
 	});
 });
 
@@ -22,6 +30,8 @@ function clearInputFields(){
 	}
 }
 
+
+//select laneId
 $(function() {
 	var laneIdInput = $("#laneIdInput");
 
@@ -30,6 +40,36 @@ $(function() {
 		var jsLaneIdInput = document.getElementById("laneIdInput");
 		let input = jsLaneIdInput.value;
 		let p = document.getElementById('foundIdsContainer');
+		//clear the found input after each keystroke
+		let foundContainer = document.getElementById('foundIdsContainer');
+		removeBadges(foundContainer);
+		//If the input is longer than 3 characters
+		if(input.length >= 3) {
+			//Filter through the lane IDS in the simulation by the given input and append them as children to the foundIdsContainer
+			let filtered = getFilteredIds(input, ids);
+			filtered.forEach(function(item) {
+				createBadgeAndAdd(item,p);
+			});
+		}
+		else if (input.length <= 2) {
+
+		}
+	}); 
+});
+
+
+//select vehicule Id
+$(function() {
+	var laneIdInput = $("#vehIdInput");
+
+	laneIdInput.keyup(function() {
+		var ids = getLanesId();
+		var jsLaneIdInput = document.getElementById("vehIdInput");
+		let input = jsLaneIdInput.value;
+		let p = document.getElementById('foundIdsContainer');
+		//clear the found input after each keystroke
+		let foundContainer = document.getElementById('foundIdsContainer');
+		removeBadges(foundContainer);
 		//If the input is longer than 3 characters
 		if(input.length >= 3) {
 			//Filter through the lane IDS in the simulation by the given input and append them as children to the foundIdsContainer
