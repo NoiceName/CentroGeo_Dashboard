a5db839e0c24c40e88e3e3f39f3147167e52673f
ReinoutVos <reinout@vos.bz>
2020-06-15 17:02:55 +0200

    added comments to code (reinout)

--- src/main/webapp/uploadzip/zip.js
+++ src/main/webapp/uploadzip/zip.js
@@ -1,3 +1,4 @@
+/*Updates the text in the upload zip box.*/
 $(function() {
 	console.log('updated21')
 	const zipFile = document.getElementById("zip-file");
@@ -12,19 +13,12 @@ $(function() {
 		}
 	});
 
-	
+/*Simulates clicking the actual button instead of the placeholder.*/	
 zipButton.addEventListener("click", function() {
 	zipFile.click();
 });
 
-zipFile.addEventListener("change", function() {
-	if(zipFile.value) {
-		customText.innerHTML = zipFile.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-	} else {
-		customText.innerHTML = "no file chosen";
-	}
-});
-
+/*Sends the zip-file to the server.*/
 $("#zipform").submit(function (evt) {
     var formData = new FormData();
     console.log(zipFile.files[0]);
