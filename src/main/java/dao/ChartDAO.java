87c4af3a93ef3a9efb2288e9e4851af27695fc68
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-06-16 18:12:41 +0200

    Started working on vehicle_information
    Cannot finish working as net.net is not uploaded

--- src/main/java/dao/ChartDAO.java
+++ src/main/java/dao/ChartDAO.java
@@ -1,5 +1,27 @@
 package dao;
 
+import java.sql.PreparedStatement;
+
+import org.json.JSONObject;
+
+import model.Database;
+
 public enum ChartDAO {
 	instance;
+	
+	
+	
+//	public JSONObject getVehicleOverTime(int simulation_id, int vehicle_id) {
+//		Database db = new Database();
+//		Database.loadPGSQL();
+//		db.connectPGSQL();
+//		String statement = "";
+//		PreparedStatement ps = db.prepareStatement();
+//	}
+
+//Unfinished code finish debeloping end point when net.net has been uploaded
+//select s.time as time, xpath('//vehicle[@id="v0"]/@speed', s.data) as speed, xpath('//vehicle[@id="v0"]/@speedFactor', s.data) as speedFactor, 
+//	from projectschema.snapshot s
+//	where s.simulation = 1
+//	order by s.time) as vehicle);
 }
--- src/main/java/resources/ChartResources.java
+++ src/main/java/resources/ChartResources.java
@@ -1,12 +1,22 @@
 package resources;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+
+import org.json.JSONObject;
 
 @Path("/simulations/{simulation_id}/charts")
 public class ChartResources {
 	
 	@PathParam("simulation_id") int simulation_id;
 
-
+	
+	
+	@GET
+	@Path("/vehicle_information")
+	public JSONObject getVehicleInformation() {
+		return new JSONObject();
+	}
+	
 }
