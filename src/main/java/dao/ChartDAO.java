8453878180b08543a91f6913857b85c7a7c43414
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-06-16 22:54:56 +0200

    Added requirements for EdgeAppereance
    Added everything to facilitate edge appereance however stil not working due to a JSON issue

--- src/main/java/dao/ChartDAO.java
+++ src/main/java/dao/ChartDAO.java
@@ -1,9 +1,14 @@
 package dao;
 
+import java.sql.Array;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.json.JSONObject;
 
+import model.EdgeAppearance;
 import model.Database;
 
 public enum ChartDAO {
@@ -24,4 +29,48 @@ public enum ChartDAO {
 //	from projectschema.snapshot s
 //	where s.simulation = 1
 //	order by s.time) as vehicle);
+	
+	/**
+	 * Generates a JSONObject
+	 * @param simulationId - ID of a simulation
+	 * @param edgeId - Id of the specified edge 
+	 * @return chart object
+	 */
+	public EdgeAppearance getEdgeAppereance(int simulationId, String edgeId) {
+		Database db = new Database();
+		Database.loadPGSQL();
+		db.connectPGSQL();
+		String statement = "select ct.time as time, count(ct.data) as counter\r\n" + 
+				"from (select s.time as time, unnest(xpath('//route/@edges', s.data))::text as data \r\n" + 
+				"from projectschema.snapshot s \r\n" + 
+				"where s.simulation = ?) as ct\r\n" + 
+				"where ct.data like '% '|| ? ||' %'\r\n" + 
+				"or (ct.data like '% ' || ?\r\n" + 
+				"or ct.data like ? || ' %')\r\n" + 
+				"group by ct.time\r\n" + 
+				"order by ct.time;";
+		PreparedStatement ps = db.prepareStatement(statement);	
+		ArrayList<JSONObject> res = new ArrayList<>();
+		try {
+			ps.setInt(1, simulationId);
+			ps.setString(2, edgeId);
+			ps.setString(3, edgeId);
+			ps.setString(4, edgeId);
+			ResultSet result = ps.executeQuery();
+			while(result.next()) {
+				int count = result.getInt("counter");
+				String time = String.valueOf(result.getInt("time"));
+				JSONObject json = new JSONObject();
+				json.put(time, count);
+				res.add(json);
+			}
+			System.out.println(res);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		EdgeAppearance chart = new EdgeAppearance(res, edgeId);
+		return chart; 
+	}
+	
 }
--- src/main/java/resources/ChartResources.java
+++ src/main/java/resources/ChartResources.java
@@ -1,10 +1,20 @@
 package resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import org.json.JSONObject;
+
+import dao.ChartDAO;
+import model.EdgeAppearance;
+
+
 
 @Path("/simulations/{simulation_id}/charts")
 public class ChartResources {
@@ -18,5 +28,22 @@ public class ChartResources {
 	public JSONObject getVehicleInformation() {
 		return new JSONObject();
 	}
-	
+
+	/**
+	 * Not working as of now, issue with JSON object conversion
+	 * @param edgeIds
+	 * @return
+	 */
+	@GET
+	@Path("/edge_appearance")
+	@Produces("application/json")
+	public ArrayList<JSONObject> getEdgeAppearance(@QueryParam("edge_ids") List<String> edgeIds){
+		ArrayList<JSONObject> res = new ArrayList<>();
+		for(String elem : edgeIds) {
+			EdgeAppearance chart = ChartDAO.instance.getEdgeAppereance(simulation_id, elem);
+			System.out.println(chart.toJSON());
+			res.add(chart.toJSON());
+		}
+		return res;
+	}
 }
