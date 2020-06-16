4a7f22a005079f776309bcafdb70a50a4c130b66
Samuel <s.f.coste@student.utwente.nl>
2020-06-16 16:18:16 +0200

    Update ChartResources.java

--- src/main/java/resources/ChartResources.java
+++ src/main/java/resources/ChartResources.java
@@ -1,12 +1,21 @@
 package resources;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+
+import org.json.JSONObject;
 
 @Path("/simulations/{simulation_id}/charts")
 public class ChartResources {
 	
 	@PathParam("simulation_id") int simulation_id;
+	
+	@GET
+	@Path("/transiting_vehicles")
+	public JSONObject getTransiting_vehicles() {
+		return new JSONObject();
+	}
 
 
 }
