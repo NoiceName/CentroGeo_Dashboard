acd1bd6cc18d9effa31fc60b08be4c56d9a35a5a
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-06-16 16:10:02 +0200

    Added ChartResourcesStub

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -11,10 +11,10 @@ public class Database {
 	//The URL of the database (Note: the url string should begin with 'jdbc::postgresq:' "
 	String url = "jdbc:postgresql://localhost:5432/";
 	//The username of the database user
-	String username = "";
+	String username = "postgres";
 	//The password of the database user
-	String password = "";
-	//Do not change this variable 
+	String password = "password";
+	//Do not change this variable
 	String schemaName = "projectschema";
 	Connection connection;
 
