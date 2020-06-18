ccfabcaa948385a8da793e3ee6ab4f46151eb300
ReinoutVos <reinout@vos.bz>
2020-06-18 12:24:50 +0200

    Revert "Revert "fixed some issues with progress bar / changed log in page""
    This reverts commit f11414452711e6cf56b60dbe03967014161c19d5.

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -9,11 +9,9 @@ import java.util.Properties;
 public class Database {
 	//Specify the parameters for the database here!
 	//The URL of the database (Note: the url string should begin with 'jdbc::postgresq:' "
-	String url = "jdbc:postgresql://localhost:5432/";
-	//The username of the database user
-	String username = "postgres";
-	//The password of the database user
-	String password = "password";
+	String url = "jdbc:postgresql://bronto.ewi.utwente.nl/";
+	String username = "dab_di19202b_234";
+	String password = "Ml+TGxyCaJ78/bwX";
 	//Do not change this variable
 	String schemaName = "projectschema";
 
