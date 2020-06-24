701846d6f880a14ae6163979be8f5ccfcc934ad0
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-06-24 15:04:20 +0200

    Finished initializiation (without DAO)

--- src/main/java/resources/InitializationResource.java
+++ src/main/java/resources/InitializationResource.java
@@ -10,7 +10,6 @@ import java.util.Properties;
 @Path("/initialize_db")
 public class InitializationResource {
 
-
    @POST
    @Consumes("application/x-www-form-urlencoded")
    public JSONObject receiveInformation(@FormParam("database_url") String dbUrl, @FormParam("database_username") String dbUsername, @FormParam("database_password") String password) {
@@ -21,17 +20,17 @@ public class InitializationResource {
       props.setProperty("user", dbUsername);
       props.setProperty("password", password);
       Connection con = null;
+      JSONObject resp = new JSONObject();
       try {
          con = DriverManager.getConnection(dbUrl, props);
          con.close();
       } catch (SQLException e) {
-         //Return the credentials are invalid
+            resp.put("result", "bad_login");
+            return resp;
       }
-      //Set database credentials here!
       Database.setUrl(dbUrl);
       Database.setPassword(password);
       Database.setUsername(dbUsername);
-      //Check if schema exists
       Database db = new Database();
       db.connectPGSQL();
       PreparedStatement st = db.prepareStatement("SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'projectschema';");
@@ -47,31 +46,93 @@ public class InitializationResource {
 
       PreparedStatement schemaSt;
       if(schemaName.equals("projectschema")){
-         //Return projectschema already exists
+          resp.put("result", "bad_exists");
       } else {
+          PreparedStatement schemaStatement = db.prepareStatement("drop schema if exists \"projectschema\" cascade;\n" +
+                  "\n" +
+                  "create schema \"projectschema\";\n" +
+                  "\n" +
+                  "create table \"projectschema\".simulation(\n" +
+                  "                                           simulation_id serial primary key,\n" +
+                  "                                           name text,\n" +
+                  "                                           date date,\n" +
+                  "                                           tags text,\n" +
+                  "                                           description text\n" +
+                  ");\n" +
+                  "\n" +
+                  "create table \"projectschema\".snapshot(\n" +
+                  "                                         snapshot_id serial primary key,\n" +
+                  "                                         simulation integer not null\n" +
+                  "                                             references \"projectschema\".simulation (simulation_id) on delete cascade,\n" +
+                  "                                         time real,\n" +
+                  "                                         data xml\n" +
+                  ");\n" +
+                  "\n" +
+                  "create table \"projectschema\".lane(\n" +
+                  "                                     lane_id text,\n" +
+                  "                                     edge_id text,\n" +
+                  "                                     simulation integer not null\n" +
+                  "                                         references \"projectschema\".simulation (simulation_id) on delete cascade,\n" +
+                  "                                     index integer,\n" +
+                  "                                     length real,\n" +
+                  "                                     shape text,\n" +
+                  "                                     primary key (lane_id, simulation)\n" +
+                  ");\n" +
+                  "\n" +
+                  "create table \"projectschema\".snapshotlane(\n" +
+                  "                                             snapshot_lane_id serial primary key,\n" +
+                  "--                                              lane text not null\n" +
+                  "--                                                  references \"projectschema\".lane (lane_id) on delete cascade,\n" +
+                  "                                             snapshot integer not null\n" +
+                  "                                                 references \"projectschema\".snapshot (snapshot_id) on delete cascade\n" +
+                  ");\n" +
+                  "\n" +
+                  "\n" +
+                  "create table \"projectschema\".user(\n" +
+                  "\n" +
+                  "                                     username text primary key,\n" +
+                  "                                     password_hash text\n" +
+                  "\n" +
+                  ");\n" +
+                  "\n" +
+                  "\n" +
+                  "create table \"projectschema\".vehicle(\n" +
+                  "                                        vehicle_id text,\n" +
+                  "                                        simulation integer not null\n" +
+                  "                                            references \"projectschema\".simulation (simulation_id) on delete cascade,\n" +
+                  "                                        depart real,\n" +
+                  "                                        primary key (vehicle_id, simulation)\n" +
+                  ");\n" +
+                  "\n" +
+                  "create table \"projectschema\".snapshotvehicle(\n" +
+                  "                                                snapshot_vehicle_id serial primary key,\n" +
+                  "--                                                 vehicle text constraint vehicle_fk not null\n" +
+                  "--                                                     references \"projectschema\".vehicle (vehicle_id, simulation) on delete cascade,\n" +
+                  "                                                snapshot integer constraint snapshot_fk not null\n" +
+                  "                                                    references \"projectschema\".snapshot (snapshot_id) on delete cascade,\n" +
+                  "                                                snapshot_lane integer not null\n" +
+                  "                                                    references \"projectschema\".snapshotlane (snapshot_lane_id) on delete cascade,\n" +
+                  "                                                simulation integer not null\n" +
+                  "                                                    references \"projectschema\".simulation (simulation_id) on delete cascade,\n" +
+                  "                                                type text,\n" +
+                  "                                                speedfactor real,\n" +
+                  "                                                state text,\n" +
+                  "                                                pos float,\n" +
+                  "                                                speed float,\n" +
+                  "                                                poslat float\n" +
+                  ");\n");
+         try {
+            schemaStatement.executeQuery();
+         } catch (SQLException throwables) {
+             System.out.println(throwables.getMessage());
+         }
       }
 
-      return null;
-
+      resp.put("result", "success");
+      return resp;
    }
 
 
 
-
-
-//      public Connection connectPGSQL() {
-//         Properties props = new Properties();
-//         props.setProperty("user", this.username);
-//         props.setProperty("password", this.password);
-//         Connection conn = null;
-//         try {
-//            conn = DriverManager.getConnection(this.url, props);
-//         } catch (SQLException e) {
-//            e.printStackTrace();
-//         }
-//         this.connection = conn;
-//         return conn;
-//
-//      }
    }
 
