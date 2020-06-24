25ea3ed8624e07e80ad951ddd6185a4dd5c1c4c6
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-06-24 15:28:06 +0200

    Defined an object to handle initilization response
    This object will  be returned upon successfull initilisation

---
+++ src/main/java/model/InitResp.java
@@ -0,0 +1,26 @@
+package model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * A response to the initialisation of the database
+ */
+@XmlRootElement
+public class InitResp {
+    String result;
+
+    public InitResp(){}
+
+    public InitResp(String res){
+        this.result = res;
+    }
+
+    public String gerResult(){
+        return this.result;
+    }
+
+    public void setResult(String res){
+        this.result = res;
+    }
+
+}
