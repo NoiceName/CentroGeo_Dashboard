82a50f78b1a8764bd843a5e117413855f760f597
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-07-01 17:39:33 +0200

    Updated the test to use the right library
    The test was using the wrong JSONObject (use the google one)

--- src/test/java/database_parameter_manager_test/DatabaseParameterManagerTest.java
+++ src/test/java/database_parameter_manager_test/DatabaseParameterManagerTest.java
@@ -1,26 +1,40 @@
 package database_parameter_manager_test;
 
 import database_parameter_manager.DatabaseParameterManager;
-import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests the DataBaseParameterManager class
  */
 public class DatabaseParameterManagerTest {
 
+    //Todo delete this test later
+    /**
+     * Check whether the program is able to test if a file exists
+     */
+//    @Test
+//    public void readTest(){
+//        System.out.println(DatabaseParameterManager.readParameters());
+//        assertTrue(true);
+//    }
+
+
     /**
      * Test to be able to write and read from a file
      */
     @Test
     public void testWriteAndReadLogin(){
-        DatabaseParameterManager.writeLogin("max", "password", "www.world.com/");
-        JSONObject userObject = DatabaseParameterManager.readLogin();
-        String password = userObject.getString("password");
-        String username = userObject.getString("username");
-        String url = userObject.getString("url");
+        DatabaseParameterManager.writeParameters("max", "password", "www.world.com/");
+        JSONArray userArr = DatabaseParameterManager.readParameters();
+        JSONObject userObject = (JSONObject)userArr.get(0);
+        String password = (String)userObject.get("password");
+        String username = (String)userObject.get("username");
+        String url = (String)userObject.get("url");
         assertEquals(password, "password");
         assertEquals(username, "max");
         assertEquals(url, "www.world.com/");
@@ -31,11 +45,13 @@ public class DatabaseParameterManagerTest {
      */
     @Test
     public void OverWriteFile(){
-        DatabaseParameterManager.writeLogin("max", "password", "www.world.com/");
-        DatabaseParameterManager.writeLogin("admin", "adminpass", "differentURL");
-        String password = userObject.getString("password");
-        String username = userObject.getString("username");
-        String url = userObject.getString("url");
+        DatabaseParameterManager.writeParameters("max", "password", "www.world.com/");
+        DatabaseParameterManager.writeParameters("admin", "adminpass", "differentURL");
+        JSONArray userArr = DatabaseParameterManager.readParameters();
+        JSONObject userObject = (JSONObject)userArr.get(0);
+        String password = (String)userObject.get("password");
+        String username = (String)userObject.get("username");
+        String url = (String)userObject.get("url");
         assertEquals(password, "adminpass");
         assertEquals(username, "admin");
         assertEquals(url, "differentURL");
