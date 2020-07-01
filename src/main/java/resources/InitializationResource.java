fabfd19ae97d783737630ea5693e1887fb0db05f
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-07-01 12:16:34 +0200

    Database initialisation interface added and tested

--- src/main/java/resources/InitializationResource.java
+++ src/main/java/resources/InitializationResource.java
@@ -23,18 +23,18 @@ public class InitializationResource {
        @Consumes("application/x-www-form-urlencoded")
        @Produces("application/json")
        public InitResp receiveInformation(@FormParam("database_url") String dbUrl, @FormParam("database_username") String dbUsername, @FormParam("database_password") String password) {
-             InitResp response = InitDAO.instance.databaseInit(dbUrl, dbUsername, password);
-             if(response.getResult().equals("success")){
-                 User admin = new User("admin", "123456");
-                 try {
-                     UserDAO.instance.insertUser(admin);
-                 } catch (Exception e) {
-                     System.out.println("Unable to create an administrator");
-                     System.out.println(e.getMessage());
-                     e.printStackTrace();
-                 }
-             }
-             return response;
+        InitResp response = InitDAO.instance.databaseInit(dbUrl, dbUsername, password);
+        if(response.getResult().equals("success")){
+            User admin = new User("admin", "123456");
+            try {
+                UserDAO.instance.insertUser(admin);
+            } catch (Exception e) {
+                System.out.println("Unable to create an administrator");
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return response;
        }
 
 
--- src/main/webapp/initialSetup/initial.js
+++ src/main/webapp/initialSetup/initial.js
@@ -1,4 +1,29 @@
 
 $(function() {
 
+    $('#send').click(function(event){
+        event.preventDefault();
+        let form = $('#initialiseDBForm');
+        $.ajax({
+                type: 'POST',
+                url: '/CentroGeo/resources/initialize_db',
+                data: form.serialize(),
+                dataType: 'json',
+                contentType: 'application/x-www-form-urlencoded',
+                success: function(resp){
+                    if(resp.result==='bad_login'){
+                        alert('The provided database information is incorrect');
+                    }
+                    else if (resp.result==='bad_exists'){
+                        alert('Database schema already exists!');
+                    }
+                    else if (resp.result==='success'){
+                        alert('Database has been successfully initiated and admin user has been set!');
+                    }
+                }
+            }
+        );
+
+    });
+
 });
