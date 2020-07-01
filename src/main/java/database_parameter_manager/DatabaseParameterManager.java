25d38b2fe380888e40f3d5c158a60b36a09839a0
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-07-01 15:21:30 +0200

    Created stubs for database parameter manager
    Added a json file that will store the login information

---
+++ src/main/java/database_parameter_manager/DatabaseParameterManager.java
@@ -0,0 +1,23 @@
+package database_parameter_manager;
+
+/**
+ * Reads the database parameters from the db_log file when the server loads
+ */
+public class DatabaseParameterManager {
+
+    /**
+     * Writes login details to a json file and saves them
+     */
+    public static void writeLogin(String username, String password, String url){}
+
+    /**
+     * Clear database parameters from the json file
+     */
+    public static void clearLogin(){}
+
+    /**
+     * Read database parameters
+     */
+    public static void readLogin(){}
+
+}
