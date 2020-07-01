c984c34a0027b3dbfe748bdad11214d3bf12f93f
Max Koval <31274864+NoiceName@users.noreply.github.com>
2020-07-01 10:25:43 +0200

    Finished db_init back-end

--- src/main/java/resources/InitializationResource.java
+++ src/main/java/resources/InitializationResource.java
@@ -12,17 +12,18 @@ import java.util.Properties;
 @Path("/initialize_db")
 public class InitializationResource {
 
-   /**
-    * Attempts to create a database.
-    * See InitDAO.databaseInit() for more detail.
-    */
-   @POST
-   @Consumes("application/x-www-form-urlencoded")
-   @Produces("application/json")
-   public InitResp receiveInformation(@FormParam("database_url") String dbUrl, @FormParam("database_username") String dbUsername, @FormParam("database_password") String password) {
-         InitResp response = InitDAO.instance.databaseInit(dbUrl, dbUsername, password);
-         return response;
-   }
+       /**
+        * Attempts to create a database.
+        * See InitDAO.databaseInit() for more detail.
+        * If a database with a "projectschema" schema already exists then it does not do anything.
+        */
+       @POST
+       @Consumes("application/x-www-form-urlencoded")
+       @Produces("application/json")
+       public InitResp receiveInformation(@FormParam("database_url") String dbUrl, @FormParam("database_username") String dbUsername, @FormParam("database_password") String password) {
+             InitResp response = InitDAO.instance.databaseInit(dbUrl, dbUsername, password);
+             return response;
+       }
 
 
 
